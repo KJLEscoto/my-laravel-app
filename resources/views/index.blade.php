3de08a9c5dc1a32f3cb4038d7d48f3c635277fd9
Kent Joemar L. Escoto <kentescoto24@gmail.com>
2025-01-20 21:23:14 +0800

    update-test

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
   <div class="flex flex-col gap-5 items-center justify-center">
-    <p class="text-center font-bold">Join our code community.</p>
+    <p class="text-center font-bold">Create notes</p>
     <a href="{{ route('show.register') }}">Register Now!</a>
   </div>
 </x-layout>
